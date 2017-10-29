fcb5aaef67cebab6c0a9d18c928b4cfeb2eb6c53
Alex Wong <alexw14@gmail.com>
2017-10-28 22:00:00 -0700

    updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
     def index
-        @Users = User.all
+        @users = User.all
     end
 
     def new
-        
+        # Provide the model instance to the form_for helper
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Thank you for signing up!"
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
