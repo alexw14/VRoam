79e70492ba9d748803a28a49af7ff29629042a68
Alex Wong <alexw14@gmail.com>
2017-10-28 23:43:58 -0700

    added sessions dir in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
-  resources :users
-  resources :orders
-  resources :products
-  resources :carts
+  resources :users, only: [:new, :create]
+  # resources :orders
+  # resources :products
+  # resources :carts
+  resources :sessions, only: [:new, :create, :destroy]
+  get "/login", to: "sessions#new"
 end
