bd95d57c8cedb1ca08c8156d00e38aeb4e86ebdc
Alex Wong <alexw14@gmail.com>
2017-11-01 13:03:16 -0700

    updated welcome index.html

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,21 +16,21 @@ products = Product.create!([
     colorful wreck of a WWII war ship. On shore, the lush jungle shelters stone temples and mischievous monkeys. The “artistic capital” of 
     Ubud is the perfect place to see a cultural dance performance, take a batik or silver-smithing workshop, or invigorate your mind and 
     body in a yoga class.", price: 4.99},
-    {id: 2, product_name: "London, United Kingdom", product_description: "The crown jewels, Buckingham Palace, Camden Market…in London, 
-    history collides with art, fashion, food, and good British ale. A perfect day is different for everyone: culture aficionados shouldn't 
-    miss the Tate Modern and the Royal Opera House. If you love fashion, Oxford Street has shopping galore. For foodies, cream tea at 
-    Harrod’s or crispy fish from a proper chippy offers classic London flavor. Music and book buffs will love seeing Abbey Road and the 
-    Sherlock Holmes Museum.", price: 3.99},
-    {id: 3, product_name: "Paris, France", product_description: "Lingering over pain au chocolat in a sidewalk café, relaxing after a day 
+    {id: 2, product_name: "Paris, France", product_description: "Lingering over pain au chocolat in a sidewalk café, relaxing after a day 
     of strolling along the Seine and marveling at icons like the Eiffel Tower and the Arc de Triomphe… the perfect Paris experience combines 
     leisure and liveliness with enough time to savor both an exquisite meal and exhibits at the Louvre. Awaken your spirit at Notre Dame, 
     bargain hunt at the Marché aux Puces de Montreuil or for goodies at the Marché Biologique Raspail, then cap it all off with a risqué 
     show at the Moulin Rouge.", price: 3.99},
-    {id: 4, product_name: "Rome, Italy", product_description: "Rome wasn't built in a day--and you'll need much more than a day to take in 
+    {id: 3, product_name: "Rome, Italy", product_description: "Rome wasn't built in a day--and you'll need much more than a day to take in 
     this timeless city. The city is a real-life collage of piazzas, open-air markets, and astonishing historic sites. Toss a coin into the 
     Trevi Fountain, contemplate the Colosseum and the Pantheon, and sample a perfect espresso or gelato before spending an afternoon shopping 
     at the Campo de’Fiori or Via Veneto. Enjoy some of the most memorable meals of your life here, too, from fresh pasta to succulent fried 
     artichokes or a tender oxtail stew.", price: 3.99},
+    {id: 4, product_name: "London, United Kingdom", product_description: "The crown jewels, Buckingham Palace, Camden Market…in London, 
+    history collides with art, fashion, food, and good British ale. A perfect day is different for everyone: culture aficionados shouldn't 
+    miss the Tate Modern and the Royal Opera House. If you love fashion, Oxford Street has shopping galore. For foodies, cream tea at 
+    Harrod’s or crispy fish from a proper chippy offers classic London flavor. Music and book buffs will love seeing Abbey Road and the 
+    Sherlock Holmes Museum.", price: 3.99},
     {id: 5, product_name: "Crete, Greece", product_description: "The birthplace of both the king of the Olympian gods and of modern European 
     civilization, Crete is a Mediterranean jewel. It’s rich with archaeological and mythological history that’s reflected in its ancient 
     ruins and cultural attractions. Soak up the charming atmosphere of Chania Town’s Old Venetian Harbor or the fortresses and monasteries 
