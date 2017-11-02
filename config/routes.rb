d67c93771457319a02a59ac00e85c05cf6c57e06
Alex Wong <alexw14@gmail.com>
2017-11-01 17:36:07 -0700

    added edit user and updated products

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
   
-  resources :users, only: [:new, :create]
+  resources :users
   resources :orders
   resources :products, only: [:index, :show]
   resources :carts, only: :index
