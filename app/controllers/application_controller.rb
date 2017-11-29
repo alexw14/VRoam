052296cc901e6fa7a27ef9aea0b2891cf7af6e08
Alex Wong <alexw14@gmail.com>
2017-11-28 18:13:57 -0800

    take out query in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   # Make the current_user and active_order method available to views, not just controllers
   helper_method :current_user
   helper_method :active_order
+  helper_method :is_product_in_cart
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -13,6 +14,10 @@ class ApplicationController < ActionController::Base
 
   def active_order
     @active_order = current_user.orders.find_by(is_active: true)
+  end
+
+  def is_product_in_cart (product)
+    Cart.where("order_id = ? AND product_id = ?", active_order.id, product.id).exists?
   end
 
   def authorize
